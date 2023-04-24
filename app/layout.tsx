32e31e02d75b3012414abd6a79ac119366091522
Andres Castro <castrogomezand@gmail.com>
2023-04-23 20:51:28 -0500

    fix: fix error in favicon :bug:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,18 @@
-import './globals.css';
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>
-        <div className="building-view">
-          {children}
-        </div>
+        <div className="building-view">{children}</div>
       </body>
     </html>
-  )
+  );
 }
